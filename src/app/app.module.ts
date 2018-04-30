6a5365cfe2fc0008e0d71ceacce442be94dc5e3e
Ihor Hrynyk <ihohry@gmail.com>
2018-04-30 20:22:40 +0300

    implement tasks component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from './login/login.component';
 import {routing } from './app.routing';
 import { ProjectsComponent } from './projects/projects.component';
 import { UserService } from './login/user.service';
+import { TasksComponent } from './tasks/tasks.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { UserService } from './login/user.service';
     AppComponent,
     NavbarComponent,
     LoginComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule, 
